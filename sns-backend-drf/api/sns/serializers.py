434ff15144b556afc9613f79c98424d4a79e190e
aho <matsue4692@gmail.com>
2024-04-13 08:45:51 +0000

    Improve: serializers.py

--- sns-backend-drf/api/sns/serializers.py
+++ sns-backend-drf/api/sns/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from api.accounts.models import User, Profile
 from api.sns.models import FriendRequest, Message
+from django.db.models import Q
 
 
 class FriendRequestSerializer(serializers.ModelSerializer):
@@ -9,7 +11,25 @@ class FriendRequestSerializer(serializers.ModelSerializer):
         extra_kwargs = {'askFrom': {'read_only': True}}
 
 
+class FriendsFilter(serializers.PrimaryKeyRelatedField):
+    '''メッセージをフレンドのみに送るためのフィルター'''
+
+    def get_queryset(self):
+        request = self.context['request']
+        friends = FriendRequest.objects.filter(Q(askTo=request.user) & Q(approved=True))
+
+        list_friend = []
+        for friend in friends:
+            list_friend.append(friend.askFrom.id)
+
+        queryset = User.objects.filter(id__in=list_friend)
+        return queryset
+
+
 class MessageSerializer(serializers.ModelSerializer):
+
+    receiver = FriendsFilter()
+
     class Meta:
         model = Message
         fields = ('id', 'message', 'receiver')
