d6c58d9a527a1ab108482dfcb32ec8b90e99ec6d
aho <matsue4692@gmail.com>
2024-04-13 08:57:23 +0000

    Update: accounts/views.py

--- sns-backend-drf/api/accounts/views.py
+++ sns-backend-drf/api/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics, authentication, permissions
+from rest_framework import viewsets
+from api.accounts import serializers
+from api.accounts.models import Profile
+from . import custompermissions
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated, custompermissions.ProfilePermission)
+
+    def perform_create(self, serializer):
+        serializer.save(userPro=self.request.user)
+
+
+class MyProfileListView(generics.ListAPIView):
+
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(userPro=self.request.user)
