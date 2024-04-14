c641a7cab442a529b2b619628d25b02508448e73
aho <matsue4692@gmail.com>
2024-04-14 11:28:02 +0000

    Update: sns/views.py urls.py

--- sns-backend-drf/api/sns/urls.py
+++ sns-backend-drf/api/sns/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from api.sns import views
+
 
 app_name = 'sns'
 
 router = DefaultRouter()
-
+router.register('approval', views.FriendRequestViewSet)
+router.register('message', views.MessageViewSet, basename="message")
+router.register('inbox', views.InboxListView, basename='inbox')
 
 urlpatterns = [
     path('', include(router.urls))
--- sns-backend-drf/api/sns/views.py
+++ sns-backend-drf/api/sns/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
-
+from rest_framework import authentication, permissions
+from django.db.models import Q
+from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework import status
+from rest_framework.response import Response
+from api.sns import serializers
+from api.sns.models import FriendRequest, Message
 # Create your views here.
+
+
+class FriendRequestViewSet(viewsets.ModelViewSet):
+    queryset = FriendRequest.objects.all()
+    serializer_class = serializers.FriendRequestSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(Q(askTo=self.request.user) | Q(askFrom=self.request.user))
+
+    def perform_create(self, serializer):
+        try:
+            serializer.save(askFrom=self.request.user)
+        except ValueError:
+            raise ValidationError("ユーザーは一意のリクエストしか持つことができません")
+
+    def destroy(self, request, *args, **kwargs):
+        """Deleteメソッドを許可しないようにする"""
+
+        response = {'message': 'Delete is not allowed !'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        """PATCHメソッドを許可しないようにする"""
+
+        response = {'message': 'Patch is not allowed !'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+
+    queryset = Message.objects.all()
+    serializer_class = serializers.MessageSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(sender=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        response = {'message': 'Delete DM is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        response = {'message': 'Update DM is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        response = {'message': 'Patch DM is not allowed'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class InboxListView(viewsets.ReadOnlyModelViewSet):
+
+    queryset = Message.objects.all()
+    serializer_class = serializers.MessageSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        return self.queryset.filter(receiver=self.request.user)
